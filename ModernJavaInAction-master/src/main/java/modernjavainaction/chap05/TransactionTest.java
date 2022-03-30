34f29c9035e7093c7d8e1f2ce3759b7a06cfcfcf
조지헌 <jojiapp@gmail.com>
2022-03-30 16:42:35 +0900

    [테스트]

--- ModernJavaInAction-master/src/main/java/modernjavainaction/chap05/TransactionTest.java
+++ ModernJavaInAction-master/src/main/java/modernjavainaction/chap05/TransactionTest.java
@@ -85,5 +85,7 @@ public class TransactionTest {
                 .map(Transaction::getValue)
                 .reduce(Integer::max)
                 .ifPresent(System.out::println);
+
+        //
     }
 }
