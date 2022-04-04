7748c10e615d4ba3727a3abb50f6f372da66fc50
조지헌 <jojiapp@gmail.com>
2022-04-05 07:14:12 +0900

    [추가] ToListCollector

--- ModernJavaInAction-master/src/main/java/modernjavainaction/chap06/ToListCollector.java
+++ ModernJavaInAction-master/src/main/java/modernjavainaction/chap06/ToListCollector.java
@@ -2,6 +2,7 @@ package modernjavainaction.chap06;
 
 import static java.util.stream.Collector.Characteristics.CONCURRENT;
 import static java.util.stream.Collector.Characteristics.IDENTITY_FINISH;
+import static modernjavainaction.chap06.Dish.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,22 +14,23 @@ import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
 
   @Override
   public Supplier<List<T>> supplier() {
-    return () -> new ArrayList<T>();
+    return ArrayList::new;
   }
 
   @Override
   public BiConsumer<List<T>, T> accumulator() {
-    return (list, item) -> list.add(item);
+    return List::add;
   }
 
   @Override
   public Function<List<T>, List<T>> finisher() {
-    return i -> i;
+    return Function.identity();
   }
 
   @Override
@@ -44,4 +46,20 @@ public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
     return Collections.unmodifiableSet(EnumSet.of(IDENTITY_FINISH, CONCURRENT));
   }
 
+}
+
+class Foo {
+  public static void main(String[] args) {
+    List<Dish> dishes1 = menu.stream().collect(new ToListCollector<>());
+    System.out.println(dishes1);
+
+    List<Dish> dishes2 = menu.stream().collect(Collectors.toList());
+    System.out.println(dishes2);
+
+    ArrayList<Object> dishes3 = menu.stream().collect(
+            ArrayList::new,
+            List::add,
+            List::addAll
+    );
+  }
 }
